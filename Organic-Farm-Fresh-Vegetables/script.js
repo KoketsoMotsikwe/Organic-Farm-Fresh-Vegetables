fa5701de88b32e805e990d0b0f7fbb6ff39a9f5e
Aneledyubhele <anele.dyubhele@capaciti.org.za>
2024-03-13 12:00:01 +0200

    changes

--- Organic-Farm-Fresh-Vegetables/script.js
+++ Organic-Farm-Fresh-Vegetables/script.js
@@ -1,19 +1,1 @@
 
-// home
-window.addEventListener('DOMContentLoaded', function() {
-
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.querySelector('.menu');
-    
-    menuToggle.addEventListener('change', function() {
-
-      if (this.checked) {
-        menu.style.display = 'block';
-      } 
-      
-      else {
-        menu.style.display = 'none';
-      }
-    });
-  });
-//   home
